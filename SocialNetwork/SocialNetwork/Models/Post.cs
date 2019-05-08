33ab6e7cc89fe8c0c84671d7ae6d4cbb63ba3076
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-05-08 12:54:13 +0200

    ln

--- SocialNetwork/SocialNetwork/Models/Post.cs
+++ SocialNetwork/SocialNetwork/Models/Post.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace SocialNetwork.Models
 {
     public class Post
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PostId { get; set; }
+        public string Content { get; set; }
+        public string Owner { get; set; }
     }
 }
