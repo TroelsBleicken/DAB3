f5f6ea0ca2ca74b21ce3a480122ae089456a8283
Andreas <201608930@post.au.dk>
2019-05-09 14:43:13 +0200

    Troels er altså kun til mænd

---
+++ SocialNetwork/SocialNetwork/Models/Detail.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models
+{
+    public abstract class Detail
+    {}
+
+    public class VideoDetail : Detail
+    {
+        public string Video { get; set; }
+    }
+
+    public class PictureDetail : Detail
+    {
+        public string Picture { get; set; }
+    }
+
+    public class PollDetail : Detail
+    {
+        public class pollOption
+        {
+            private string option;
+            private long votes;
+
+            public pollOption(string opt)
+            {
+                option = opt;
+                votes = 0;
+            }
+
+            public void vote()
+            {
+                votes++;
+            }
+
+            public void unVote()
+            {
+                votes--;
+            }
+        }
+
+        public List<pollOption> Poll { get; set; }
+    }
+}
