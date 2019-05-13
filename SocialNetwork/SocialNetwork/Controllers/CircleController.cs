95895ebcb664cd2fcb30309260ba09a0017e337d
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-05-13 08:28:04 +0200

    Circles kører

---
+++ SocialNetwork/SocialNetwork/Controllers/CircleController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.DAL;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Controllers
+{
+    public class CircleController : Controller
+    {
+        // GET: Circle
+        private CircleRepository _circleRepository;
+        public CircleController(CircleRepository circleRepository)
+        {
+            _circleRepository = circleRepository;
+        }
+        public ActionResult Index()
+        {
+            return View(_circleRepository.GetCircles());
+        }
+
+        // GET: Circle/Details/5
+        public ActionResult Details(string id)
+        {
+            return View(_circleRepository.GetCircleById(id));
+        }
+
+        // GET: Circle/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Circle/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Circle circle)
+        {
+            try
+            {
+                _circleRepository.InsertCircle(circle);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Circle/Edit/5
+        public ActionResult Edit(string id)
+        {
+            return View(_circleRepository.GetCircleById(id));
+        }
+
+        // POST: Circle/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, IFormCollection collection)
+        {
+            try
+            {
+                
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Circle/Delete/5
+        public ActionResult Delete(string id)
+        {
+            return View(_circleRepository.GetCircleById(id));
+        }
+
+        // POST: Circle/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string id, IFormCollection collection)
+        {
+            try
+            {
+                _circleRepository.RemoveCircle(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SocialNetwork/SocialNetwork/DAL/CircleRepository.cs
+++ SocialNetwork/SocialNetwork/DAL/CircleRepository.cs
@@ -2,10 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using SocialNetwork.Models;
 
 namespace SocialNetwork.DAL
 {
     public class CircleRepository
     {
+        private readonly IMongoCollection<Circle> _circles;
+
+        public CircleRepository()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("SocialNetworkDb");
+            _circles = database.GetCollection<Circle>("Circles");
+        }
+
+        public List<Circle> GetCircles()
+        {
+            return _circles.Find(c => true).ToList();
+        }
+
+        public Circle GetCircleById(string id)
+        {
+            return _circles.Find(c => c.CircleId == id).FirstOrDefault();
+        }
+
+        public void InsertCircle(Circle circle)
+        {
+            _circles.InsertOne(circle);
+        }
+
+        public void UpdateCircle(Circle circle)
+        {
+            _circles.ReplaceOne(c => c.CircleId == circle.CircleId, circle);
+        }
+
+        public Circle GetCircleByName(string name)
+        {
+            return _circles.Find(c => c.Name == name).FirstOrDefault();
+        }
+
+        public void RemoveCircle(string id)
+        {
+            _circles.DeleteOne(c => c.CircleId == id);
+        }
     }
+    
+
 }
