129373ae8102963eb44c5888bd3b300931f38476
Andreas <201608930@post.au.dk>
2019-05-15 09:18:53 +0200

    ting til comment

---
+++ SocialNetwork/SocialNetwork/DAL/CommentRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.DAL
+{
+    public class CommentRepository
+    {
+        private readonly IMongoCollection<Comment> _comments;
+
+        public CommentRepository(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("SocialNetworkDb"));
+            var database = client.GetDatabase("SocialNetworkDb");
+
+            if (!MongoDbHelpFunctions.CollectionExists(database, "Comments"))
+                database.CreateCollection("Comments");
+
+            _comments = database.GetCollection<Comment>("Comments");
+        }
+        
+        public List<Comment> GetComments()
+        {
+            return _comments.Find(comment => true).ToList();
+        }
+
+        public Comment GetComment(string id)
+        {
+            return _comments.Find<Comment>(comment => comment.CommentId == id).FirstOrDefault();
+        }
+
+        public Comment CreateComment(Comment comment)
+        {
+            _comments.InsertOne(comment);
+            return comment;
+        }
+
+        public void UpdateComment(string id, Comment commentIn)
+        {
+            var filter = Builders<Comment>.Filter.Eq(c => c.CommentId, id);
+            var result = _comments.ReplaceOne(filter, commentIn, new UpdateOptions { IsUpsert = true });
+        }
+   
+        public void RemoveComment(Comment commentIn)
+        {
+            _comments.DeleteOne(comment => comment.CommentId == commentIn.CommentId);
+        }
+
+        public void RemoveComment(string id)
+        {
+            _comments.DeleteOne(comment => comment.CommentId == id);
+        }
+    }
+}
